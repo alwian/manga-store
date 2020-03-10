2fbe4aa85ef9a864b8dc0370ee65baca3739365c
Jiashu Li <js803931@dal.ca>
2020-03-10 11:30:35 -0300

    Added login functionality

--- login.php
+++ login.php
@@ -1,3 +1,41 @@
+<?php
+    include_once 'config/Database.php';
+    include_once 'models/User.php';
+    $errorMsg = null;
+
+    //Login
+    //check if the the form been submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        //check all the required information is been set and not empty
+        if(isset($_POST['email']) && !empty($_POST['email'])
+            && isset($_POST['password']) && !empty($_POST['password'])
+            ){
+            //connect to db
+            $db = new Database();
+            $user = new User($db->connect());
+            //check if the email is exists
+            $user->email = $_POST['email'];
+            if($user->exists()!=null){
+                //check if the password matches the database
+                $user->password = $_POST['password'];
+                //check the password from db
+                if($user->checkLogin()){
+                    $_SESSION['email'] = $user->email;
+                    $_SESSION['Logged'] = true;
+                    header("Location: index.php");
+                }else{
+                    $errorMsg = "Sorry, the login information is incorrect.";
+                }
+
+            }else{
+                $errorMsg = "Sorry, the login information is incorrect.";
+            }
+        }else{
+            $errorMsg = "Sorry, some of the field is still empty.";
+        }
+    }
+
+?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -19,17 +57,18 @@
     <div class="page-container">
         <h2>Login</h2>
 
-        <form class="forms">
+        <form class="forms" action="login.php" method="post">
             <div class="form-group">
                 <label for="InputEmail">Email address</label>
-                <input type="email" class="form-control" id="InputPassword" aria-describedby="emailHelp" placeholder="Email">
+                <input type="email" class="form-control" id="InputPassword" name="email" aria-describedby="emailHelp" placeholder="Email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
                 <label for="InputPassword">Password</label>
-                <input type="password" class="form-control" id="InputPassword" placeholder="Password">
+                <input type="password" class="form-control" id="InputPassword" name="password" placeholder="Password">
             </div>
             <button type="submit" class="btn btn-primary" id="login-button">Submit</button>
+            <p class="form-text text-muted"><?php echo$errorMsg ?></p>
         </form>
     </div>
 
