c84891100fb49ac110132270ac1b2121f00178d5
Alex Anderson <alex@alwian.com>
2020-03-30 14:48:09 +0100

    Added Database.php back is it was deleted from develop

---
+++ config/Database.php
@@ -0,0 +1,22 @@
+<?php
+class Database
+{
+    private $dbname = 'csci3172g1';
+    private $host = '';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname . ';', $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            //echo $e->getMessage();
+            return null;
+        }
+        return $this->conn;
+    }
+}
