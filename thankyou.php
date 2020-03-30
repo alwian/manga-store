e005782cd5799c4f0ff103a687e3f58675f92235
ccheung <ch675393@dal.ca>
2020-03-30 19:56:13 -0300

    Created thank you page that indicates purchase was confirmed

---
+++ thankyou.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Import Stylesheets-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php require "header.php";?>
+
+<h1 class="text-center">Thank you for your purchase. We have received your order.</h1><br><br>
+
+</body>
+</html>
