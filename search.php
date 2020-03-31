7e3f2b0917abe816f4868531c146ef5bf26527b2
Amanda Norman <am252599@dal.ca>
2020-03-31 19:31:36 +0100

    Some tweak edits made to the Search page to work
    Removed the search bar from the search page. Changed the search span in header
    to a button so it can submit a POST request. Edited some of the PHP for the search function.
    Added "no results" text if no matches are found.
    Made the CSS style the same as homepage.

--- search.php
+++ search.php
@@ -1,11 +1,18 @@
 <?php
-session_start();
-include 'config/Database.php';
-include 'models/Item.php';
-$value = $_POST['search'];
-$mysql = new Database();
-$conn = $mysql->connect();
-$query = "SELECT * from `items` where UPPER(name) like UPPER(:name) ";
+    session_start();
+    include 'config/Database.php';
+    include 'models/Item.php';
+
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $value = $_POST['search'];
+    }
+    else{
+        $value = "";
+    }
+    
+    $mysql = new Database();
+    $conn = $mysql->connect();
+    $query = "SELECT * from `items` where UPPER(name) like UPPER(:name) ";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,35 +31,26 @@ $query = "SELECT * from `items` where UPPER(name) like UPPER(:name) ";
 
 <body>
     <?php require "header.php"; ?>
-    <div class="container">
-        <form class="input-group flex-nowrap pt-3" action="search.php" method="POST">
-            <input type="text"
-                class="form-control"
-                value="<?php echo $value; ?>"
-                name="search">
-                <span>
-                <svg id="search-img" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                    <path d="M0 0h24v24H0V0z" fill="none" />
-                    <path
-                        d="M15.5 14h-.79l-.28-.27c1.2-1.4 1.82-3.31 1.48-5.34-.47-2.78-2.79-5-5.59-5.34-4.23-.52-7.79 3.04-7.27 7.27.34 2.8 2.56 5.12 5.34 5.59 2.03.34 3.94-.28 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-                </svg>
-            </span>
-        </form>
+    <div class="page-container">
+        <h2>Search Results for: <?php echo $value ?></h2>
         <div class="items">
-
-        <?php
-$statement = $conn->prepare($query);
-$statement->bindValue(":name", "%{$value}%");
-$statement->execute();
-$result = $statement->fetchAll();
-foreach ($result as $row) {
-    $bk = new Item($conn);
-    $bk->db_construct($row);
-    $bk->displayCard();
-}
-
-?>
-</div>
+            <?php
+                $stmt = $conn->prepare($query);
+                $stmt->bindValue(":name", "%{$value}%");
+                $stmt->execute();
+                $result = $stmt->fetchAll();
+                if(sizeOf($result) != 0){
+                    foreach ($result as $row) {
+                        $item = new Item($conn);
+                        $item->db_construct($row);
+                        $item->displayCard();
+                    }
+                }
+                else{
+                    echo "No results. Try a different search";
+                }
+            ?>
+        </div>
     </div>
     <!--JavaScript at end of body for optimized loading-->
     <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
