abb2950ccbf8838b5c42bfd606a2ec6a2a50e156
Amanda Norman <am252599@dal.ca>
2020-03-31 17:27:58 +0100

    Styled book page and added 'add to cart' function
    Added css for book page. Created page layout. Managed PHP to allow
    access to data from the tables and pages. Created function to add Item
    to the cart. Fixed the SQL error in the cart when adding items.

--- models/Item.php
+++ models/Item.php
@@ -13,6 +13,7 @@ class Item
     public $image;
     public $description;
     public $average_rating;
+    public $number_pages;
 
     function __construct($conn)
     {
@@ -58,7 +59,7 @@ class Item
     }
 
     public function getItem() {
-        $query = "SELECT item_id, seller_id, name, author, price, stock, image, description, total_rating / rating_count as average_rating FROM $this->table WHERE item_id = :item_id";
+        $query = "SELECT item_id, seller_id, name, author, price, stock, image, description, round(total_rating / rating_count * 100, 2) as average_rating, number_pages FROM $this->table WHERE item_id = :item_id";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":item_id", $this->item_id);
 
@@ -72,6 +73,7 @@ class Item
             $stmt->bindColumn('image', $this->image);
             $stmt->bindColumn('description', $this->description);
             $stmt->bindColumn('average_rating', $this->average_rating);
+            $stmt->bindColumn('number_pages', $this->number_pages);
             if ($stmt->rowCount() === 1) {
                 $stmt->fetch(PDO::FETCH_BOUND);
                 return true;
--- page.php
+++ page.php
@@ -1,51 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
+    <?php
+        session_start();
 
-<?php
-session_start();
+        require 'config/Database.php';
+        require 'models/User.php';
+        require 'models/Item.php';
 
-require 'config/Database.php';
-require 'models/Item.php';
+        $db = new Database();
+        $conn = $db->connect();
 
-$db = new Database();
-$conn = $db->connect();
+        $item = new Item($conn);
 
-$item = new Item($conn);
+        $id = $_GET["id"];
+        $item->item_id = $id;
 
-$id = $_GET["id"];
-$item->item_id = $id;
+        if (!$item->getItem()) {
+            echo 'Could not find the specified item.';
+            exit;
+        }
 
-if (!$item->getItem()) {
-    echo 'Could not find the specified item.';
-    exit;
-}
-?>
-<head>
-    <meta charset="UTF-8">
-    <meta name="referrer" content="no-referrer" />
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $item->author;?></title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import bootstrap.css-->
-    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" media="screen,projection" />
-    <link type="text/css" rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<?php
-require "header.php";
+        $seller = new User($db->connect());
+        $seller->user_id = $item->seller_id;
+        $seller->getUser();
+    ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="referrer" content="no-referrer" />
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $item->author;?></title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import bootstrap.css-->
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" media="screen,projection" />
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <?php
+            require "header.php";
 
-echo 'Name: ' . $item->name . '<br><br>';
-echo 'Document Author: ' . $item->author . '<br><br>';
+            $content = <<<EOD
+            '<div class="page-container" id="item-page">
+                <div id="item-info">
+                    <h2 id="page-title">{$item->name}</h2>
+                    <h5 id="page-author">Author: <em>{$item->author}</em></h5>
+                    </br>
+                    <div id="item-details">
+                        <div id="page-image" width="25%">
+                            <img src="data/product-images/{$item->image}" referrerpolicy="no-referrer"/>
+                        </div>
+                        <div id="page-extras">
+                            <h5>Number of Pages in Comic: <em>{$item->number_pages}</em></h5>
+                            <h5>Rating: <em>{$item->average_rating}</em></h5>
+                            </br>
+                            <h5>Sold by: <em>{$seller->first_name} {$seller->last_name}</em></h5>
+                            <h5>Stock available: <em>{$item->stock}</em></h5>
+                        </div>
+                    </div>
+                    </br>
+                    <div id="page-description">
+                        <h5>Description:</h5>
+                        <p>{$item->description}</p>
+                    </div>
+                </div>
+                <div id="item-purchase">
+                    <h4><b>Purchase</b></h4>
+                    </br>
+                    <h5 id="price_label"><b>Price:</b></h5>
+                    <h3 id="price_value">&#36;{$item->price}</h3>
+                    </br>
+                    <form class="forms" action="cart.php" method="post">
+                        <label for="selectQuantity">Quantity:</label>
+                        <input type="number" name="quantity" id="selectQuantity" value="1"/>
+                        <input type="hidden" name="item_id" value="{$id}"/>
+                        <button class="btn btn-primary" id="cart-btn" type="submit">Add to Cart</button>
+                    </form>
+                </div>
+            </div>'
+EOD;
+            echo $content;
+        ?>
 
-echo '<br>';
-echo $item->description . '<br>';
-?>
-
-<img src="data/product-images/<?php echo $item->image?>" referrerpolicy='no-referrer'/>
-
-<script type="text/javascript" src="js/jquery-3.4.1.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/script.js"></script>
-</body>
+        <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    </body>
 </html>
