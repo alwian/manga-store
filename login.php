75f9376d7f9a1e6039a8d9d04b6b83d544393282
Amanda Norman <am252599@dal.ca>
2020-04-01 02:16:49 +0100

    Fixed login styling issues

--- login.php
+++ login.php
@@ -64,12 +64,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <h2>Login</h2>
 
     <form class="forms" action="login.php" method="post">
-        <div class="form-group">
+        <div class="form-group form-login">
             <label for="InputEmail">Email address</label>
             <input type="email" class="form-control" id="InputEmail" name="email" aria-describedby="emailHelp" placeholder="Email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-        <div class="form-group">
+        <div class="form-group form-login">
             <label for="InputPassword">Password</label>
             <input type="password" class="form-control" id="InputPassword" name="password" placeholder="Password">
         </div>
