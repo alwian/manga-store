56806492f8fa2033cdb4211ace464ecccaaeeb72
Alex Anderson <alex@alwian.com>
2020-04-21 16:13:35 +0100

    Removed 403 response

--- util/Response.php
+++ util/Response.php
@@ -3,7 +3,6 @@ class Response
 {
     public static $BAD_REQUEST = 400;
     public static $UNAUTHORIZED = 401;
-    public static $FORBIDDEN = 403;
     public static $NOT_FOUND = 404;
     public static $CONFLICT = 409;
     public static $INTERNAL_SERVER_ERROR = 500;
