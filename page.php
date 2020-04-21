5a3d1c6e1ba758ee36cc7cea945c1b4197d9c06f
Alex Anderson <alex@alwian.com>
2020-04-21 15:40:15 +0100

    Added contains method to view page

--- page.php
+++ page.php
@@ -185,16 +185,8 @@ if ($item->stock > 0) {
 }
 $content .= "</form>";
 
-$wishlist = new Wishlist($conn);
-$wishlist->user_id = $_SESSION['id'];
-$wishlist_items = $wishlist->getItems();
-$already_in_wishlist = false;
-foreach ($wishlist_items as $wishlist_item) {
-    if ($wishlist_item['item_id'] == $item_id) {
-        $already_in_wishlist = true;
-        break;
-    }
-}
+
+$already_in_wishlist = Wishlist::containsItem($conn, $_SESSION['id'], $_POST['item_id']);
 if (!$already_in_wishlist) {
     $content .= "<form action='page.php' method='post'>
 <input type=\"hidden\" name=\"item_id\" value=\"{$item_id}\"/>
